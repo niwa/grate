e6cb9bc98b57dc40ffbd57cf7f839a005c5b57fd
Matt Wilkins <matt.wilkins@niwa.co.nz>
2026-07-16 15:25:04 +1200

    Tidy up warning message

--- bin/updates.py
+++ bin/updates.py
@@ -82,10 +82,10 @@ def possibly_update():
         return
 
     if version.parse(cver) >= version.parse(ivers[0]["version"]):
-        print("WARNING: {cver} is already update!")
+        print(f"WARNING: {cver} is already up to date!")
 
     # update to ivers[0]
-    r = input(f"Install version {ivers[0]['version']}? [y/N]: ").strip().lower()
+    r = input(f"Install newest version {ivers[0]['version']}? [y/N]: ").strip().lower()
 
     if r != "y":
         return
